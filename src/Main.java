b8b41088cdd80783d676138d6d106542309b392e
kavyavignan99 <32007923+kavyavignan99@users.noreply.github.com>
2017-09-16 13:20:47 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		StudentGroup l = new StudentGroup(5);
+
+ student[] s = new student[5];
+ for(i=0; i<5; i++)
+{
+  s[i]=new student("
+
+		Student r = new Student(01,"kavya",new Date(1996,07,11),63.568);
+		Student r1 = new Student(02,"rani",new Date(1996,06,06),63.568);
+		Student r2 = new Student(03,"raj",new Date(1996,02,05),63.568);
+		Student r3 = new Student(04,"sita",new Date(1996,05,17),63.568);
+		Student r4 = new Student(05,"ravi",new Date(1996,02,10),63.568);
+
+
+	}
+
+}
