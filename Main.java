109a3fbe68cd29b01d352779c3e97a41375767aa
kavyavignan99 <32007923+kavyavignan99@users.noreply.github.com>
2017-09-16 13:15:04 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
-
 import java.util.Date;
+
 public class Main {
-public static void main(String[]args){
-StudentGroup stg=new StudentGroup(10);
-Date d=new Date(1999,9,10);
-Student s1=new Student(20,"abc",d,89.0);
-stg.addFirst(s1);
-System.out.println(stg.getStudent(0).getId());
-}
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		StudentGroup l = new StudentGroup(5);
+
+ student[] s1 = new student[5];
+ for(i=0 i<5; i++)
+{
+  s1[i]=new student("
+
+		Student r = new Student(01,"kavya",new Date(1996,07,11),63.568);
+		Student r1 = new Student(02,"rani",new Date(1996,06,06),63.568);
+		Student r2 = new Student(03,"raj",new Date(1996,02,05),63.568);
+		Student r3 = new Student(04,"sita",new Date(1996,05,17),63.568);
+		Student r4 = new Student(05,"ravi",new Date(1996,02,10),63.568);");
+
+
+	}
+
 }
--- StudentGroup.java
+++ StudentGroup.java
@@ -1,237 +1,230 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
-/**
-* An interface that provide a number of operations for any array of students
-*
-*/
-public class StudentArrayOperation {
-/**
-* Returns the array of students
-*
-* @return the array of students
-*
-*/
-Student[] getStudents();
-/**
-* Sets the array of students
-* if students == null method should throw IllegalArgumentException
-*
-* @param students
-*
-* @throws IllegalArgumentException
-*/
-void setStudents(Student[] students);
-/**
-* if index lower than 0 or index higher/equal students.length method should
-* throw IllegalArgumentException
-*
-* @param index
-*/
-Student getStudent(int index);
-/**
-* Replaces the element at the specified position in this array with the
-* specified element. if student == null method should throw
-* IllegalArgumentException if index lower than 0 or index higher/equal
-* students.length method should throw IllegalArgumentException
-*
-* @param student
-* the student to be stored at the specified position
-* @param index
-* the index of the element to replace
-*
-*
-* @throws IllegalArgumentException
-*/
-void setStudent(Student student, int index);
-/**
-* Appends the specified element to the specified position of this array if
-* student == null method should throw IllegalArgumentException if index
-* lower than 0 or index higher/equal students.length method should throw
-* IllegalArgumentException
-*
-* @param student
-* the element to be appended to this array at the specified
-* position
-* @param index
-* the specified position
-*
-* @throws IllegalArgumentException
-*/
-void add(Student student, int index);
-/**
-* Inserts the specified element at the beginning of this array if student
-* == null method should throw IllegalArgumentException
-*
-* @param student
-* the element to add
-*
-* @throws IllegalArgumentException
-*/
-void addFirst(Student student);
-/**
-* Appends the specified element to the end of this array. if student ==
-* null method should throw IllegalArgumentException
-*
-* @param student
-* the element to add
-*
-* @throws IllegalArgumentException
-*/
-void addLast(Student student);
-/**
-* Removes the element at the specified position in this array if index
-* lower than 0 or index higher/equal students.length method should throw
-* IllegalArgumentException
-*
-* @param index
-* the index of the element to be removed
-*
-* @throws IllegalArgumentException
-*/
-void remove(int index);
-/**
-* Removes the first occurrence of the specified element from this array, if
-* it is present. If this array does not contain the element, it is
-* unchanged and should throw IllegalArgumentException with "Student not
-* exist" message if student == null method should throw
-* IllegalArgumentException
-*
-* @param student
-* the element to remove
-*
-* @throws IllegalArgumentException
-*/
-void remove(Student student);
-/**
-* Removes all elements from the specified index (except the element with
-* specified index) from this array if index lower than 0 or index
-* higher/equal students.length method should throw IllegalArgumentException
-*
-* @param index
-* the index elements from should be removed
-*
-* @throws IllegalArgumentException
-*/
-void removeFromIndex(int index);
-/**
-* Removes all elements from the specified element (except the specified
-* element) from this array if student == null method should throw
-* IllegalArgumentException
-*
-* @param student
-* the element from elements should be removed
-*
-* @throws IllegalArgumentException
-*/
-void removeFromElement(Student student);
-/**
-* Removes all elements to the specified index (except the element with the
-* specified index) from this array if index lower than 0 or index
-* higher/equal students.length method should throw IllegalArgumentException
-*
-* @param index
-* the index elements to should be removed
-*
-* @throws IllegalArgumentException
-*/
-void removeToIndex(int index);
-/**
-* Removes all elements to the specified element (except the specified
-* element) from this array if student == null method should throw
-* IllegalArgumentException
-*
-* @param student
-* the element to elements should be removed
-*
-* @throws IllegalArgumentException
-*/
-void removeToElement(Student student);
-/**
-* Performs Bubble Sort to this array
-*
-*/
-void bubbleSort();
-/**
-* Returns all students with birthday to the specified date if date == null,
-* method should throw IllegalArgumentException
-*
-* @param date
-* the date that specify birthday to find students who was born
-* before that date and in that date
-*
-* @return the array of students who was born before/in specified date
-*
-* @throws IllegalArgumentException
-*/
-Student[] getByBirthDate(Date date);
-/**
-* Returns all students with birthday to the specified range of dates
-* including specified dates if firstDate == null or lastDate == null,
-* method should throw IllegalArgumentException
-*
-* @param firstDate
-* the date that specify birthday to find students from
-* @param lastDate
-* the date that specify birthday to find students to
-*
-* @return the array of students who was born in specified range of dates
-*
-* @throws IllegalArgumentException
-*/
-Student[] getBetweenBirthDates(Date firstDate, Date lastDate);
-/**
-* Returns all students with birthday to the specified date and in several
-* days after the specified date if date == null, method should throw
-* IllegalArgumentException
-*
-* @param date
-* the date that specify birthday to find students from
-* @param days
-* the number of days that specify the end date
-*
-* @return the array of students who has birthday in specified date and
-* several days after
-*
-* @throws IllegalArgumentException
-*/
-Student[] getNearBirthDate(Date date, int days);
-/**
-* Find student at the specified position and calculate his age (total
-* years) if indexOfStudent == 0, method should throw
-* IllegalArgumentException
-*
-* @param indexOfStudent
-* the index to find student and calculate age
-*
-* @return student full years
-*
-* @throws IllegalArgumentException
-*/
-int getCurrentAgeByDate(int indexOfStudent);
-/**
-* Returns all students with age equal the specified age
-*
-* @param age the age to find students
-*
-* @return the array of students of the specified age
-*/
-Student[] getStudentsByAge(int age);
-/**
-* Finds the maximum average mark for all students in this array
-* and returns all students who has the maximum average mark
-*
-* @return the array of students who has maximum average mark
-*
-*/
-Student[] getStudentsWithMaxAvgMark();
-/**
-* Finds element which is the next to the specified element
-* if student == null, method should throw IllegalArgumentException
-*
-* @param student the element to which take the next element
-*
-* @throws IllegalArgumentException
-*/
-Student getNextStudent(Student student);
-}
 
+/**
+ * A fix-sized array of students
+ * array length should always be equal to the number of stored elements
+ * after the element was removed the size of the array should be equal to the number of stored elements
+ * after the element was added the size of the array should be equal to the number of stored elements
+ * null elements are not allowed to be stored in the array
+ *
+ * You may add new methods, fields to this class, but DO NOT RENAME any given class, interface or method
+ * DO NOT PUT any classes into packages
+ *
+ */
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+
+	/**
+	 * DO NOT remove or change this constructor, it will be used during task check
+	 * @param length
+	 */
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+	}
+
+	@Override
+	public Student[] getStudents() {
+		// Add your implementation here
+		//Correct
+		return students;
+	}
+
+	@Override
+	public void setStudents(Student[] students) {
+		// Add your implementation here
+		if (students == null) {
+			throw new IllegalArgumentException();
+		}
+		else this.students = students;
+	}
+
+	@Override
+	public Student getStudent(int index) {
+		// Add your implementation here
+		if(index < 0 || index >= students.length) throw new IllegalArgumentException();
+		else return students[index];
+	}
+
+	@Override
+	public void setStudent(Student student, int index) {
+		// Add your implementation here
+		if(index < 0 || index >= students.length) throw new IllegalArgumentException();
+		else {
+			students[index] = student;
+		}
+	}
+
+	@Override
+	public void addFirst(Student student) {
+		// Add your implementation here
+		//Still to be Reviewed
+		if (student == null ) throw new IllegalArgumentException();
+		else {
+			ArrayList <Student>at = new ArrayList<Student>();
+			at = (ArrayList<Student>) Arrays.asList(students);
+			at.add(0, student);
+			students = null;
+			students = (Student[]) at.toArray();
+
+			/*
+			Student []st = new Student[students.length+1];
+			st[0] = student;
+			for(int i=1;i<st.length;i++) {
+				st[i] = students[i-1];
+			}
+			students  = null;
+			Student []students = new Student[st.length];
+			for(int i=0;i<st.length;i++) {
+				students[i] = st[i];
+			}*/
+		}
+	}
+
+	@Override
+	public void addLast(Student student) {
+		// Add your implementation here
+		//Still to be Reviewed
+		if (student == null ) throw new IllegalArgumentException();
+		else {
+			ArrayList <Student>at = new ArrayList<Student>();
+			at = (ArrayList<Student>) Arrays.asList(students);
+			at.add(students.length, student);
+			students = null;
+			students = (Student[]) at.toArray();
+		}
+
+	}
+
+	@Override
+	public void add(Student student, int index) {
+		// Add your implementation here
+
+
+	}
+
+	@Override
+	public void remove(int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void remove(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeFromIndex(int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeFromElement(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeToIndex(int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void removeToElement(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void bubbleSort() {
+		// Add your implementation here
+		Arrays.sort(students);
+	}
+
+	@Override
+	public Student[] getByBirthDate(Date date) {
+		// Add your implementation here
+		if (date == null) throw new IllegalArgumentException();
+		else {
+			Student []st = new Student[students.length];
+			int count =0;
+			for(int i=0;i<students.length;i++)
+			if(students[i].getBirthDate().compareTo(date) == 0) {
+				st[count] = students[i];
+				count++;
+			}
+			Student []ts = new Student[count];
+
+			for(int i=0;i<count;i++) {
+				ts[i] = st[i];
+			}
+			return ts;
+		}
+
+	}
+
+	@Override
+	public Student[] getBetweenBirthDates(Date firstDate, Date lastDate) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public Student[] getNearBirthDate(Date date, int days) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public int getCurrentAgeByDate(int indexOfStudent) {
+		// Add your implementation here
+		return 0;
+	}
+
+	@Override
+	public Student[] getStudentsByAge(int age) {
+		// Add your implementation here
+		/*Student st[] = new Student[students.length];
+		int count=0;
+		for(int i=0;i<students.length;i++) if(students[i].getBirthDate()getClass().getAge() == age) {
+			st[count] = students[i];
+			count++;
+		}*/
+		return null;
+	}
+
+	@Override
+	public Student[] getStudentsWithMaxAvgMark() {
+		// Add your implementation here
+		double d = 0;
+		for(int i=0;i<students.length;i++) {
+
+			if(students[i].getAvgMark() > d) {
+				d = students[i].getAvgMark();
+			}
+		}
+		Student []st = new Student[students.length];
+		int count=0;
+		for(int i=0;i<students.length;i++) {
+			if(d == students[i].getAvgMark()) {st[count] = students[i]; count++;}
+		}
+		Student []ts = new Student[count];
+		for(int i=0;i<count;i++) {
+			ts[i] = st[i];
+		}
+		return ts;
+	}
+
+	@Override
+	public Student getNextStudent(Student student) {
+		//Correct
+		// Add your implementation here
+		if (student == null) throw new IllegalArgumentException();
+		else
+		for(int i=0;i<students.length;i++) {
+			if(student.getId() == students[i].getId()) return students[i+1];
+		}
+		return null;
+	}
+}
